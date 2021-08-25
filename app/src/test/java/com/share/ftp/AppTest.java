340e0402078ea2f72a2598bede4f1ea40378ebad
minsungbyun <enjoy8939@naver.com>
2021-08-25 19:37:41 +0900

    21/08/25 main page update

--- app/src/test/java/com/share/ftp/AppTest.java
+++ app/src/test/java/com/share/ftp/AppTest.java
@@ -4,11 +4,8 @@
 package com.share.ftp;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test public void appHasAGreeting() {
+  }
 }
