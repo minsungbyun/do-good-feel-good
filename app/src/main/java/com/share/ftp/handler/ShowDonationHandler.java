36e366b7d5e803d91cfbc7b9a1c06e1f4c077304
Leejiho-940915 <Leejiho-940915@daum.net>
2021-08-28 22:03:14 +0900

    handler&domain class update

---
+++ app/src/main/java/com/share/ftp/handler/ShowDonationHandler.java
@@ -0,0 +1,12 @@
+package com.share.ftp.handler;
+
+public class ShowDonationHandler {
+
+  public void list() {
+    System.out.println("기부 목록");
+  }
+
+  public void detail() {
+    System.out.println("기부 상세보기");
+  }
+}
