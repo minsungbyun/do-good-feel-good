b2474f6f66d10737cc42e42bda72724d8a7ddafe
wejugnuk <uk20915@naver.com>
2021-08-28 22:17:03 +0900

    .

---
+++ app/src/main/java/com/share/ftp/domain/myQuestionListDTO.java
@@ -0,0 +1,14 @@
+package com.share.ftp.domain;
+
+import java.sql.Date;
+
+public class myQuestionListDTO {
+  public int no;
+  public String title;
+  public String memberld;
+  public int password;
+  public String content;
+  public String fileUpload;
+  public Date registeredDate;
+  public int viewCount;
+}
---
+++ app/src/main/java/com/share/ftp/handler/FaqListHandler.java
@@ -0,0 +1,20 @@
+package com.share.ftp.handler;
+
+public class FaqListHandler {
+
+  public void NoticeList() {
+    System.out.println("");
+  }
+  public void NoticeDetail() {
+    System.out.println();
+  }
+}
+
+
+
+
+
+
+
+
+
