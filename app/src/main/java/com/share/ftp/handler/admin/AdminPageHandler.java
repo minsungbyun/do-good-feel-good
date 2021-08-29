9a10adaac8f87c5110b0be88c9803d792612be73
minsungbyun <enjoy8939@naver.com>
2021-08-29 21:52:13 +0900

    21/08/29 main page & sub page connect

--- app/src/main/java/com/share/ftp/handler/admin/AdminPageHandler.java
+++ app/src/main/java/com/share/ftp/handler/admin/AdminPageHandler.java
@@ -7,16 +7,18 @@ public class AdminPageHandler {
 
   // 봉사활동 관련 Handler
   PersonalVolRequestHandler personalVolRequestHandler;
-  ShowVolHandler showVolHandler = new ShowVolHandler(personalVolRequestHandler);
+  ShowVolHandler showVolHandler;
 
-  public AdminPageHandler(PersonalVolRequestHandler personalVolRequestHandler) {
+  public AdminPageHandler(PersonalVolRequestHandler personalVolRequestHandler,ShowVolHandler showVolHandler) {
     this.personalVolRequestHandler = personalVolRequestHandler;
+    this.showVolHandler = showVolHandler;
   }
 
 
 
   public void showMember() {
     System.out.println("회원정보 조회");
+    personalVolRequestHandler.applyList();
   }
 
   public void showDonation() {
@@ -32,6 +34,7 @@ public class AdminPageHandler {
       System.out.println("2. 기관봉사신청내역");
       System.out.println("3. 승인하기");
       System.out.println("4. 반려하기");
+      System.out.println("5. 개인폼(테스트)");
       System.out.println("0. 이전메뉴");
 
       int menuNo = Prompt.inputInt("봉사활동관리> ");
@@ -40,6 +43,7 @@ public class AdminPageHandler {
         case 2: showVolHandler.orgApprove(); break;
         case 3: showVolHandler.accept(); break;
         case 4: showVolHandler.reject(); break;
+        case 5: personalVolRequestHandler.applyList(); break;
         case 0: return;
         default:
           System.out.println("무효한 메뉴 번호입니다.");
