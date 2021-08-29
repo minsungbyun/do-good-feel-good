6a3bc6bd5832f28fd19af0310f1a5b5e65825206
baekyeon9 <dev.baekyeon@gmail.com>
2021-08-29 13:51:52 +0900

    21/08/29 LoginHandler

---
+++ app/src/main/java/com/share/ftp/handler/personal/FacebookLoginHandler.java
@@ -0,0 +1,15 @@
+package com.share.ftp.handler.personal;
+
+
+public class FacebookLoginHandler {
+  public void showAPI() {
+    System.out.println("페이스북로그인");
+  }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/share/ftp/handler/personal/IdFindHandler.java
@@ -0,0 +1,15 @@
+package com.share.ftp.handler.personal;
+
+
+public class IdFindHandler {
+  public void findPw() {
+    System.out.println("비밀번호찾기");
+  }
+}
+
+
+
+
+
+
+
