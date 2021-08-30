b510e08da10f86176618b066f795a00e314a29f0
wejugnuk <uk20915@naver.com>
2021-08-30 23:41:43 +0900

    21/08/30

--- app/src/main/java/com/share/ftp/handler/personal/QuestionListHandler.java
+++ app/src/main/java/com/share/ftp/handler/personal/QuestionListHandler.java
@@ -6,15 +6,13 @@ import com.share.util.Prompt;
 
 public class QuestionListHandler {
 
-
   static final int MAX_LENGTH = 5;
   MyQuestionListDTO[] myQuestionLists = new MyQuestionListDTO[MAX_LENGTH];
   int size = 0;
 
-
   public void addQuestionlist() {
 
-    System.out.println("[게시글 등록하기]");
+    System.out.println("[고객센터/문의하기/문의하기 등록]");
 
     MyQuestionListDTO myQuestionList = new MyQuestionListDTO();
 
@@ -29,9 +27,9 @@ public class QuestionListHandler {
   }
 
   public void questionList() {
-    System.out.println("[게시글 목록]");
+    System.out.println("[고객센터/문의하기/문의하기 목록]");
     for (int i = 0; i < this.size; i++) {
-      System.out.printf("%d, %s, %s, %d, %s, %s, %s, %d\n", 
+      System.out.printf("%d, %s, %s, %s, %s, %s, %s, %d\n", 
           this.myQuestionLists[i].getNo(), 
           this.myQuestionLists[i].getTitle(), 
           this.myQuestionLists[i].getMemberld(),
@@ -45,7 +43,7 @@ public class QuestionListHandler {
   }
 
   public void detailQuestion() {
-    System.out.println("[게시글 상세보기]");
+    System.out.println("[고객센터/문의하기/문의하기 상세]");
     int no = Prompt.inputInt("번호? ");
 
     MyQuestionListDTO myQuestionList = findByNo(no);
@@ -59,7 +57,9 @@ public class QuestionListHandler {
     System.out.printf("내용: %s\n", myQuestionList.getContent());
     System.out.printf("작성자: %s\n", myQuestionList.getMemberld());
     System.out.printf("등록일: %s\n", myQuestionList.getRegisteredDate());
-    System.out.printf("조회수: %d\n", ++myQuestionList.getViewCount());
+
+    myQuestionList.setViewCount(myQuestionList.getViewCount() + 1);
+    System.out.printf("조회수: %d\n", myQuestionList.getViewCount());
   }
 
   public void updateQuestion() {
@@ -73,8 +73,8 @@ public class QuestionListHandler {
       return;
     }
 
-    String title = Prompt.inputString(String.format("제목(%s)? ", myQuestionList.title));
-    String content = Prompt.inputString(String.format("내용(%s)? ", myQuestionList.content));
+    String title = Prompt.inputString(String.format("제목(%s)? ", myQuestionList.getTitle()));
+    String content = Prompt.inputString(String.format("내용(%s)? ", myQuestionList.getContent()));
 
     String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
     if (input.equalsIgnoreCase("n") || input.length() == 0) {
@@ -82,25 +82,18 @@ public class QuestionListHandler {
       return;
     }
 
-    myQuestionList.title = title;
-    myQuestionList.content = content;
+    myQuestionList.setTitle(title);
+    myQuestionList.setContent(content);
     System.out.println("게시글을 변경하였습니다.");
   }
 
   public void deleteQuestion() {
-    System.out.println("[게시글 삭제]");
+    System.out.println("[고객센터/문의하기/문의하기 삭제]");
     int no = Prompt.inputInt("번호? ");
 
-    int boardIndex = -1;
+    int index = indexOf(no);
 
-    for (int i = 0; i < this.size; i++) {
-      if (this.myQuestionLists[i].no == no) {
-        boardIndex = i;
-        break;
-      }
-    }
-
-    if (boardIndex == -1) {
+    if (index == -1) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
     }
@@ -111,7 +104,7 @@ public class QuestionListHandler {
       return;
     }
 
-    for (int i = boardIndex + 1; i < this.size; i++) {
+    for (int i = index + 1; i < this.size; i++) {
       this.myQuestionLists[i - 1] = this.myQuestionLists[i];
     }
     this.myQuestionLists[--this.size] = null;
@@ -119,12 +112,22 @@ public class QuestionListHandler {
     System.out.println("게시글을 삭제하였습니다.");
   }
 
+  private MyQuestionListDTO findByNo(int no) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.myQuestionLists[i].getNo() == no) {
+        return this.myQuestionLists[i];
+      }
+    }
+    return null;
+  }
+
+  private int indexOf(int no) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.myQuestionLists[i].getNo() == no) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
 }
-
-
-
-
-
-
-
-
