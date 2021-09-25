83fe80546e79379083565f632188272f482542b4
wejugnuk <uk20915@naver.com>
2021-09-25 14:29:19 +0900

    21/09/25 project study010

--- app/src/main/java/com/share/ftp/domain/admin/QuestionDTO.java
+++ app/src/main/java/com/share/ftp/domain/admin/QuestionDTO.java
@@ -8,17 +8,17 @@ import java.util.Objects;
 public class QuestionDTO implements Serializable {
 
   private int no;
-  private int adminId;
   private String title;
+  private String adminId;
+  private int password;
   private String content;
   private String fileUpload;
   private Date registerDate;
   private int viewCount;
 
-
   @Override
   public int hashCode() {
-    return Objects.hash(adminId, content, fileUpload, no, registerDate, title, viewCount);
+    return Objects.hash(adminId, content, fileUpload, no, password, registerDate, title, viewCount);
   }
   @Override
   public boolean equals(Object obj) {
@@ -29,16 +29,16 @@ public class QuestionDTO implements Serializable {
     if (getClass() != obj.getClass())
       return false;
     QuestionDTO other = (QuestionDTO) obj;
-    return adminId == other.adminId && Objects.equals(content, other.content)
+    return Objects.equals(adminId, other.adminId) && Objects.equals(content, other.content)
         && Objects.equals(fileUpload, other.fileUpload) && no == other.no
-        && Objects.equals(registerDate, other.registerDate) && Objects.equals(title, other.title)
-        && viewCount == other.viewCount;
+        && password == other.password && Objects.equals(registerDate, other.registerDate)
+        && Objects.equals(title, other.title) && viewCount == other.viewCount;
   }
   @Override
   public String toString() {
-    return "QuestionDTO [no=" + no + ", adminId=" + adminId + ", title=" + title + ", content="
-        + content + ", fileUpload=" + fileUpload + ", registerDate=" + registerDate + ", viewCount="
-        + viewCount + "]";
+    return "QuestionDTO [no=" + no + ", title=" + title + ", adminId=" + adminId + ", password="
+        + password + ", content=" + content + ", fileUpload=" + fileUpload + ", registerDate="
+        + registerDate + ", viewCount=" + viewCount + "]";
   }
   public int getNo() {
     return no;
@@ -46,17 +46,23 @@ public class QuestionDTO implements Serializable {
   public void setNo(int no) {
     this.no = no;
   }
-  public int getAdminId() {
-    return adminId;
-  }
-  public void setAdminId(int adminId) {
-    this.adminId = adminId;
-  }
   public String getTitle() {
     return title;
   }
   public void setTitle(String title) {
     this.title = title;
+  }
+  public String getAdminId() {
+    return adminId;
+  }
+  public void setAdminId(String adminId) {
+    this.adminId = adminId;
+  }
+  public int getPassword() {
+    return password;
+  }
+  public void setPassword(int password) {
+    this.password = password;
   }
   public String getContent() {
     return content;
