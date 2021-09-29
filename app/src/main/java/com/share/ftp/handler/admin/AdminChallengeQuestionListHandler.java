1bd3f2dd8e8e16d36e5199fff7fcd44c3ff369ab
SeongjunP <guppy217@naver.com>
2021-09-29 19:08:11 +0900

    21/09/29 challenge reply update

--- app/src/main/java/com/share/ftp/handler/admin/AdminChallengeQuestionListHandler.java
+++ app/src/main/java/com/share/ftp/handler/admin/AdminChallengeQuestionListHandler.java
@@ -1,15 +1,14 @@
-package com.share.ftp.handler.personal.challenge;
+package com.share.ftp.handler.admin;
 
 import java.util.List;
 import com.share.ftp.domain.admin.ChallengeDTO;
 import com.share.ftp.domain.personal.ChallengeQuestionDTO;
 import com.share.ftp.handler.CommandRequest;
+import com.share.ftp.handler.personal.challenge.AbstractChallengeQuestionHandler;
 import com.share.util.Prompt;
 
-public class ChallengeQuestionListHandler extends AbstractChallengeQuestionHandler {
-
-
-  public ChallengeQuestionListHandler(List<ChallengeQuestionDTO> challengeQuestionDTOList,
+public class AdminChallengeQuestionListHandler extends AbstractChallengeQuestionHandler {
+  public AdminChallengeQuestionListHandler(List<ChallengeQuestionDTO> challengeQuestionDTOList,
       List<ChallengeDTO> challengeDTOList, List<ChallengeQuestionDTO> challengeReplyList) {
     super(challengeQuestionDTOList, challengeDTOList, challengeReplyList);
   }
@@ -25,36 +24,42 @@ public class ChallengeQuestionListHandler extends AbstractChallengeQuestionHandl
 
       if (challengeQuestionDTOList.isEmpty()) {
         System.out.println("문의가 없습니다.");
+        return;
       }
 
       for (ChallengeQuestionDTO challengeQuestionDTO : challengeQuestionDTOList) {
-        if (challengeQuestionDTO.getNo() == challengeNo && !challengeQuestionDTO.getOwner().getId().equals("admin")) {
+        if (challengeQuestionDTO.getNo() == challengeNo) {
           System.out.printf("%d, %d, %s, %s, %s\n", 
               challengeQuestionDTO.getNo(),
               challengeQuestionDTO.getQuestionNo(),
               challengeQuestionDTO.getOwner().getId(),
-              challengeQuestionDTO.getTitle(),
+              challengeQuestionDTO.getContent(),
               challengeQuestionDTO.getRegisteredDate());
+        } else {
+          System.out.println("문의가 없습니다.");
+          return;
         }
       }
 
-      for (ChallengeQuestionDTO challengeReply : challengeReplyList) {
-        if (challengeReply.getOwner().getId().equals("admin")) {
-          System.out.printf("관리자 :  %s, %s, %s\n", 
-              challengeReply.getOwner().getId(),
-              challengeReply.getContent(),
-              challengeReply.getRegisteredDate());
-        }
-      }
+      //      for (ChallengeQuestionDTO challengeReply : challengeReplyList) {
+      //        if (challengeReply.getOwner().getId().equals("admin")) {
+      //          System.out.printf("%d, %d, %s, %s, %s\n", 
+      //              challengeReply.getNo(),
+      //              challengeReply.getQuestionNo(),
+      //              challengeReply.getOwner().getName(),
+      //              challengeReply.getContent(),
+      //              challengeReply.getRegisteredDate());
+      //        }
+      //      }
 
       System.out.println();
-      System.out.println("1번 ▶ 문의 등록");
+      System.out.println("1번 ▶ 답글 등록, 변경, 삭제");
       System.out.println("2번 ▶ 문의 변경, 삭제");
       System.out.println("3번 ▶ 문의 검색");
       System.out.println("0번 ▶ 이전");
       int input = Prompt.inputInt("번호 입력 ▶ ");
       switch (input) {
-        case 1: request.getRequestDispatcher("/challengeQuestion/add").forward(request); break;
+        case 1: request.getRequestDispatcher("/adminChallenge/replyConnect").forward(request); break;
         case 2: request.getRequestDispatcher("/challengeQuestion/connect").forward(request); break;
         case 3: request.getRequestDispatcher("/challengeQuestion/search").forward(request); break;
         case 0: return;
