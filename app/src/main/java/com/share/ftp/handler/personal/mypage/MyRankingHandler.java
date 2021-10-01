dd90643d00b44ace94be0fcbea54371972c40503
minsungbyun <enjoy8939@naver.com>
2021-10-01 09:51:57 +0900

    21/10/01 change ranking

--- app/src/main/java/com/share/ftp/handler/personal/mypage/MyRankingHandler.java
+++ app/src/main/java/com/share/ftp/handler/personal/mypage/MyRankingHandler.java
@@ -1,10 +1,14 @@
 package com.share.ftp.handler.personal.mypage;
 
+import static com.share.util.Prompt.getUserLevel;
+import static com.share.util.Prompt.getUserPoint;
+import static com.share.util.Prompt.getUserRemainPoint;
 import static com.share.util.Prompt.printMyRank;
 import java.util.List;
 import com.share.ftp.domain.join.JoinDTO;
 import com.share.ftp.handler.Command;
 import com.share.ftp.handler.CommandRequest;
+import com.share.ftp.handler.join.AuthLoginHandler;
 
 public class MyRankingHandler implements Command {
 
@@ -16,9 +20,16 @@ public class MyRankingHandler implements Command {
 
   @Override
   public void execute(CommandRequest request) throws Exception {
-    System.out.println(); 
-    System.out.println("나의 랭킹 보기");
-    System.out.println(); 
+
+    JoinDTO loginUser = AuthLoginHandler.getLoginUser();
+
+    if (loginUser == null) {
+      System.out.println("로그인 하지 않았습니다.");
+      return;
+    }
+    System.out.println();
+    System.out.printf("[ %s님의 랭킹 정보입니다 ]\n", loginUser.getName());
+    System.out.println();
 
     System.out.println("    __  ___         ____              __   _            \r\n"
         + "   /  |/  /_  __   / __ \\____ _____  / /__(_)___  ____ _\r\n"
@@ -28,7 +39,13 @@ public class MyRankingHandler implements Command {
         + "       /____/                                  /____/   ");
     System.out.println();
 
-    System.out.printf("▶ 축하합니다! 당신의 랭킹은 %d등입니다. ",printMyRank(joinDTOList)); 
+    System.out.printf("▶ 축하합니다! 당신의 랭킹은 %d등입니다.\n ",printMyRank(joinDTOList)); 
+    System.out.println();
+    System.out.printf("▶ 당신의 현재 등급은 %s입니다. \n", getUserLevel(loginUser)); 
+    System.out.println();
+    System.out.printf("▶ 당신의 현재 포인트는 %d점 입니다. \n", getUserPoint(loginUser));
+    System.out.println();
+    System.out.printf("▶ 다음 등급까지 %d point 남았습니다. ", getUserRemainPoint(loginUser)); 
     System.out.println();
 
   }
