00f5c1fbb6453fd78874c820154b63aef694e9ac
minsungbyun <enjoy8939@naver.com>
2021-10-04 22:34:44 +0900

    21/10/04 add challengeDao challengeReviewDao challengeQuestionDao

--- app-client/src/main/java/com/share/ftp/dao/ChallengeDao.java
+++ app-client/src/main/java/com/share/ftp/dao/ChallengeDao.java
@@ -2,26 +2,17 @@ package com.share.ftp.dao;
 
 import java.util.List;
 import com.share.ftp.domain.admin.ChallengeDTO;
-import com.share.ftp.domain.join.JoinDTO;
 
 public interface ChallengeDao {
 
+  // 챌린지 관련
+  void insert(ChallengeDTO addChallenge) throws Exception;      
+  List<ChallengeDTO> findAll() throws Exception;      
+  void update(ChallengeDTO updateChallenge) throws Exception;          
+  void delete(int deleteChallengeNo) throws Exception;          
 
-  ChallengeDTO findByChallengeNo(int no) throws Exception;
+  ChallengeDTO findByChallengeNo(int challengeNo) throws Exception;
   String getRemainTime(long millis) throws Exception;
-  int getNextNum() throws Exception;            // 회원 고유번호 지정
-  void insert(ChallengeDTO challengeDTO) throws Exception;          // 회원 추가
-  List<ChallengeDTO> findAll() throws Exception;               // 회원 목록
-
-  void update(ChallengeDTO challengeDTO) throws Exception;          // 회원 수정
-  void delete(ChallengeDTO challengeDTO) throws Exception;          // 회원 삭제
-  JoinDTO selectOneByIdPassword(String userId, String userPassword) throws Exception; // 로그인 유효성 검사
-  JoinDTO selectOneByIdEmail(String userId, String userEmail) throws Exception; // 로그인 유효성 검사
-  JoinDTO selectOneByUser(String userId, String userPassword) throws Exception; // 회원정보 상세보기
-  JoinDTO selectOneByEmail(String userEmail) throws Exception; // 회원정보 상세보기
-  JoinDTO selectOneByTel(String userTel) throws Exception; // 회원정보 상세보기
-  JoinDTO validId(ChallengeDTO challengeDTO) throws Exception;               // 아이디 유효성 검사
-
-
+  int getNextNum() throws Exception;       
 
 }
--- app-client/src/main/java/com/share/menu/Menu.java
+++ app-client/src/main/java/com/share/menu/Menu.java
@@ -2,13 +2,13 @@ package com.share.menu;
 
 public abstract class Menu {
 
-  public static final int ACCESS_LOGOUT = 0x01; //      비회원
-  public static final int ACCESS_PERSONAL = 0x02; //    개인회원                0010
-  public static final int ACCESS_ORG = 0x04;    //      기관회원                0100      
-  public static final int ACCESS_MEMBER = 0x08;  //     개인회원 + 기관회원     1000
-  public static final int ACCESS_ADMIN = 0x10; //       관리자
-  public static final int ACCESS_MEMBER_ADMIN = 0x20;  // 개인회원 + 기관회원 + 관리자
-  public static final int ACCESS_ALL = 0x40;  // 개인회원 + 기관회원 + 관리자
+  public static final int ACCESS_LOGOUT = 0x01;         // 비회원
+  public static final int ACCESS_PERSONAL = 0x02;       // 개인회원                0010
+  public static final int ACCESS_ORG = 0x04;            // 기관회원                0100      
+  public static final int ACCESS_MEMBER = 0x08;         // 개인회원 + 기관회원     1000
+  public static final int ACCESS_ADMIN = 0x10;          // 관리자
+  public static final int ACCESS_MEMBER_ADMIN = 0x20;   // 개인회원 + 기관회원 + 관리자
+  public static final int ACCESS_ALL = 0x40;            // 개인회원 + 기관회원 + 관리자
 
   String title;
 
@@ -23,7 +23,9 @@ public abstract class Menu {
     this.accessScope = accessScope;
   }
 
+  public int getAccessScope() {
+    return accessScope;
+  }
 
   public abstract void execute();
-
 }
