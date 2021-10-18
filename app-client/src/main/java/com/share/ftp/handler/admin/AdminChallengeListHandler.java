ffdd90f1bdff5f92bbedab05e6d76a62b1e36d06
SeongjunP <guppy217@naver.com>
2021-10-19 00:53:24 +0900

    21/10/19 MariadbChallengeDao update

--- app-client/src/main/java/com/share/ftp/handler/admin/AdminChallengeListHandler.java
+++ app-client/src/main/java/com/share/ftp/handler/admin/AdminChallengeListHandler.java
@@ -22,12 +22,12 @@ public class AdminChallengeListHandler implements Command {
     Collection<ChallengeDTO> challengeList = challengeDao.findAll();
 
     for (ChallengeDTO challengeDTO : challengeList) {
-      System.out.printf("%d, %s[%d], %s, %d, %s ~ %s\n", 
+      System.out.printf("%d, %s[%d], %d, %s ~ %s\n", 
           challengeDTO.getNo(), 
           //          challengeDTO.getAdminId(), 
           challengeDTO.getTitle(), 
           challengeDTO.getReviewCount(), 
-          challengeDTO.getAdmin().getName(),
+          //          challengeDTO.getAdmin().getName(),
           challengeDTO.getTotalJoinCount(),
           //          challengeDTO.getFileUpload(), 
           challengeDTO.getStartDate(),
