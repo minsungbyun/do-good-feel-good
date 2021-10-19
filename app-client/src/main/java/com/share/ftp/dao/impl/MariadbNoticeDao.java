cdb88773608b2da3a73aaeb838184bdfda7a3335
wejugnuk <uk20915@naver.com>
2021-10-19 14:03:54 +0900

    21/10/19 Mariadb

---
+++ app-client/src/main/java/com/share/ftp/dao/impl/MariadbNoticeDao.java
@@ -0,0 +1,56 @@
+//package com.share.ftp.dao.impl;
+//
+//import java.sql.Connection;
+//import java.util.List;
+//import com.share.ftp.dao.NoticeDao;
+//import com.share.ftp.domain.admin.NoticeDTO;
+//
+//// 게시글 데이터를 서버를 통해 관리한다.
+//
+//public class MariadbNoticeDao implements NoticeDao {
+//
+//  Connection con;
+//
+//  public MariadbNoticeDao(Connection con) {
+//    this.con = con;
+//  }
+//
+//  @Override
+//  public void insert(NoticeDTO addNotice) throws Exception {
+//
+//  }
+//  @Override
+//  public List<NoticeDTO> findAll() throws Exception {
+//
+//    return null;
+//  }
+//
+//  @Override
+//  public List<NoticeDTO> findByKeyword(String keyword) throws Exception {
+//    return null;
+//  }
+//
+//  @Override
+//  public NoticeDTO findByNoticeNo(int noticeNo) throws Exception {
+//    return null;
+//  }
+//
+//
+//  @Override
+//  public void update(NoticeDTO updateNotice) throws Exception {
+//
+//
+//  }
+//
+//  @Override
+//  public void delete(int noticeNo) throws Exception {
+//
+//  }
+//
+//  @Override
+//  public int getNextNum() throws Exception {
+//    return null;
+//
+//  }
+//
+//}
