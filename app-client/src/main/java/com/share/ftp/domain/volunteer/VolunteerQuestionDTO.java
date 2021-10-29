03486abc7ab584646caec366e8f5f4db5e88edd2
minsungbyun <enjoy8939@naver.com>
2021-10-29 17:56:27 +0900

    21/10/29 update exerd & dao

--- app-client/src/main/java/com/share/ftp/domain/volunteer/VolunteerQuestionDTO.java
+++ app-client/src/main/java/com/share/ftp/domain/volunteer/VolunteerQuestionDTO.java
@@ -17,78 +17,62 @@ public class VolunteerQuestionDTO implements Serializable {
   private Date registeredDate;
   private JoinDTO owner;
   private String reply;
-
-
-
-
-  public int getVolunteerNo() {
-    return volunteerNo;
-  }
-
-  public void setVolunteerNo(int volunteerNo) {
-    this.volunteerNo = volunteerNo;
-  }
-
-
-  public int getGetContentCount() {
-    return getContentCount;
-  }
-
-  public int getGetReplyCount() {
-    return getReplyCount;
-  }
-
   public int getQuestionNo() {
     return questionNo;
   }
-
   public void setQuestionNo(int questionNo) {
     this.questionNo = questionNo;
   }
-
+  public int getVolunteerNo() {
+    return volunteerNo;
+  }
+  public void setVolunteerNo(int volunteerNo) {
+    this.volunteerNo = volunteerNo;
+  }
+  public int getGetContentCount() {
+    return getContentCount;
+  }
+  public void setGetContentCount(int getContentCount) {
+    this.getContentCount = getContentCount;
+  }
+  public int getGetReplyCount() {
+    return getReplyCount;
+  }
+  public void setGetReplyCount(int getReplyCount) {
+    this.getReplyCount = getReplyCount;
+  }
   public String getTitle() {
     return title;
   }
-
   public void setTitle(String title) {
     this.title = title;
   }
-
   public String getContent() {
     return content;
   }
-
   public void setContent(String content) {
     this.content = content;
   }
-
   public Date getRegisteredDate() {
     return registeredDate;
   }
-
   public void setRegisteredDate(Date registeredDate) {
     this.registeredDate = registeredDate;
   }
-
   public JoinDTO getOwner() {
     return owner;
   }
-
   public void setOwner(JoinDTO owner) {
     this.owner = owner;
   }
-
   public String getReply() {
     return reply;
   }
-
   public void setReply(String reply) {
     this.reply = reply;
   }
 
-  public static long getSerialversionuid() {
-    return serialVersionUID;
-  }
+
 
 
 
